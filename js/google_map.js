cc12503e28d8955afdd64dfc8be4db623aceccb9
kocmant <t.kocmancz@gmail.com>
2017-08-17 15:57:13 +0200

    Footer improved

--- js/google_map.js
+++ js/google_map.js
@@ -4,6 +4,21 @@ function googleMap () {
 	let map = new google.maps.Map(document.getElementById('google-map'), {
 		center: myLatLng,
 		zoom: 15,
+		zoomControl: true,
+		zoomControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_CENTER
+		},
+		mapTypeControl: true,
+		mapTypeControlOptions: {
+			style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+			position: google.maps.ControlPosition.TOP_CENTER
+		},
+		scaleControl: true,
+		streetViewControl: true,
+		streetViewControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_BOTTOM
+		},
+		fullscreenControl: true
 	});
 
 	let marker = new google.maps.Marker({
